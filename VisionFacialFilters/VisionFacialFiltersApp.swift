36f5613d57a9f4d7e64a383577f23ef5ddca22cd
João Brentano <joaobrentano.br@gmail.com>
2022-05-04 16:15:43 -0300

    Initial Commit

---
+++ VisionFacialFilters/VisionFacialFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionFacialFiltersApp.swift
+//  VisionFacialFilters
+//
+//  Created by João Brentano on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct VisionFacialFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
